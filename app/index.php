95c2a9b556e1af3b7aff26a34e6dcae56abe408d
taballa-hp-mini <taballa@gmail.com>
2013-01-28 22:32:43 +0800

    update

--- app/index.php
+++ app/index.php
@@ -1,6 +1,13 @@
 <?php
 // framework/app/index.php
- 
+include_once '../vendor/autoload.php';
+
 $input = isset($_GET['name']) ? $_GET['name'] : 'World';
+
+header('Content-type: text/html; charset=utf-8');
  
-printf('Hello %s', $input);
+printf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8'));
+
+
+
+
