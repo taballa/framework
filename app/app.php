4d7ff8b1adebbbb6a8957043a8814baadf987ab6
taballa-hp-mini <taballa@gmail.com>
2013-02-03 22:25:24 +0800

    add HttpKernel; found framework Simplex and Calender

--- app/app.php
+++ app/app.php
@@ -4,18 +4,6 @@ use Symfony\Component\Routing;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel;
 
-class LeapYearController
-{
-    public function indexAction($year)
-    {
-        if (is_leap_year($year)) {
-            return new Response('Yep, this is a leap year!');
-        }
- 
-        return new Response('Nope, this is not a leap year.');
-    }
-}
-
 function render_template($request)
 {
     extract($request->attributes->all(), EXTR_SKIP);
@@ -24,14 +12,6 @@ function render_template($request)
  
     return new Response(ob_get_clean());
 } 
-
-function is_leap_year($year = null) {
-    if (null === $year) {
-        $year = date('Y');
-    }
- 
-    return 0 == $year % 400 || (0 == $year % 4 && 0 != $year % 100);
-}
 
 $routes = new Routing\RouteCollection();
 $routes->add('hello', new Routing\Route('/hello/{name}', array(
@@ -57,7 +37,7 @@ $routes->add('bye', new Routing\Route('/bye', array(
 )));
 $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
     'year' => null,
-    '_controller' => 'LeapYearController::indexAction',
+    '_controller' => 'Calendar\\Controller\\LeapYearController::indexAction',
 )));
 return $routes; 
 return $routes;
